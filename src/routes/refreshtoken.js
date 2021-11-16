5e206ca84e745cc90050a4075ec6f449b6810552
Michael <clonex@live.dk>
2021-11-16 15:25:22 +0100

    Added refreshtoken-endpoint

--- src/routes/refreshtoken.js
+++ src/routes/refreshtoken.js
@@ -1,19 +1,18 @@
 export default {
     type: "post",
-    path: "/auth/login",
+    path: "/auth/refreshtoken",
     auth: false,
     callback: async (req, res) => {
         let ret = {
             error: true,
         };
-        const resp = await Rapid.publish("signIn", "signIn-response", {
-            username: req.body.username ?? "",
-            password: req.body.password ?? "",
+        const resp = await Rapid.publish("accessToken", "accessToken-response", {
+            refreshToken: req.body.refreshToken ?? "",
             sessionID: res.locals.sessionID,
         });
         if(resp && resp.token)
         {
-            resp.accessToken = resp.token;
+            resp.refreshToken = resp.token;
             resp.error = false;
         }
     
